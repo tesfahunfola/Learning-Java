c07e111206b46cb15b030c4dde21baaee54b86a5
Tesfahun Fola <folatesfahun@gmail.com>
2025-09-26 14:57:58 -0700

    My New update

---
+++ src/main/java/com/pluralsight_week_1/MethodExercise.java
@@ -0,0 +1,49 @@
+package com.pluralsight_week_1;
+
+import java.util.Scanner;
+
+public class MethodExercise {
+    public static void main(String[] args) {
+        printWelcome();
+        displayPlayerName("Tes");
+        showLevel(4);
+        String man = getPlayerTitle("TEsfahun");
+        System.out.println(man);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Who is attacker? ");
+        String attackerPer = scan.nextLine();
+        System.out.println("Who is target? ");
+        String targetPer = scan.nextLine();
+        attack(attackerPer, targetPer);
+
+
+    }
+    public static void printWelcome(){
+        System.out.println("Welcome to Code Quest!");
+    }
+    public static void displayPlayerName(String playerName){
+        System.out.printf("Player joined: %s \n",playerName);
+    }
+ //public static void showLevel(int level)
+    //Write a static method that prints:
+    //
+    //"Level: [level]"
+    //→ Call it twice with different levels.
+
+    public static void showLevel(int level){
+        System.out.println("Level: "+ level);
+    }
+
+    public static void attack(String attacker, String target){
+
+        System.out.printf("%s attacks %s", attacker, target);
+    }
+    //. public static String getPlayerTitle(String name)
+    //Return a string like:
+    //"Lord " + name or "Princess " + name"
+    //→ Call it in main() and print the result.
+    public static String getPlayerTitle(String name){
+        return name;
+    }
+}

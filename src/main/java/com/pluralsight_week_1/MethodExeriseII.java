5d5d4fc8871590623d2f7b0c37954e3e26c2a1a9
Tesfahun Fola <folatesfahun@gmail.com>
2025-09-29 02:02:48 -0700

    My New update

---
+++ src/main/java/com/pluralsight_week_1/MethodExeriseII.java
@@ -0,0 +1,50 @@
+package com.pluralsight_week_1;
+
+import java.util.Scanner;
+
+public class MethodExeriseII {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hat do you want to do (1-add, 2- subtract) ?");
+        int num = scanner.nextInt();
+
+
+
+        if (num ==1){
+            System.out.println("1. Add");
+            doAdd(scanner);
+        }else if(num == 2 ){
+            System.out.println("Your choice is 2: subtract");
+            doSubtract(scanner);
+        }else{
+            System.out.println("Invalid input: "+num);
+        }
+
+
+
+    }
+
+    public static void doAdd(Scanner scanner) {
+
+        System.out.print("Enter 1st number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter 2nd number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+        System.out.printf("%f + %f = %f", num1, num2, sum);
+    }
+    public static void doSubtract(Scanner scanner) {
+
+        System.out.print("Enter 1st number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter 2nd number: ");
+        double num2 = scanner.nextDouble();
+
+        double difference = num1 - num2;
+        System.out.printf("%f - %f = %f", num1, num2, difference);
+    }
+
+}

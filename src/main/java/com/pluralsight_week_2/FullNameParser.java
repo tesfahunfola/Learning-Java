896a6664023ca3ce0dc21d87b83809eddc3d5a15
Tesfahun Fola <folatesfahun@gmail.com>
2025-09-30 15:26:54 -0700

    My New update

--- src/main/java/com/pluralsight_week_2/FullNameParser.java
+++ src/main/java/com/pluralsight_week_2/FullNameParser.java
@@ -6,13 +6,47 @@ import java.util.Scanner;
 public class FullNameParser {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter your name: ");
-        String name = scanner.nextLine();
-        String trimname= name.trim();
+        System.out.print("Enter your full name: ");
+        String fullNameParser2 = scanner.nextLine();
+        fullNameParser2(fullNameParser2);
+    } public static void fullNameParser2(String name) {
+        name = name.trim();
 
-        String [] fullname = trimname.trim().split("\\s+");
-        for (String splitName : fullname){
-            System.out.println(splitName);
+        // variables for name parts
+        String firstName = "";
+        String middleName = "(none)";
+        String lastName = "";
+
+        // splitting the name and store in an array
+        String[] arrFullName = name.split("\\s+");
+
+        // check the length of the array
+        // if length equal to 3, populate all the fields
+        // else if length equal to 2, populate first name and last name
+        // else invalid input
+
+        if(arrFullName.length  == 3) {
+            firstName = arrFullName[0];
+            middleName = arrFullName[1];
+            lastName = arrFullName[2];
+        } else if(arrFullName.length  == 2) {
+            firstName = arrFullName[0];
+            lastName = arrFullName[1];
+        } else {
+            System.out.println("Invalid name format!");
+            return;
         }
+
+        // print the name
+        printName(firstName, middleName, lastName);
+
+    }
+
+    public static void printName(String firstName, String middleName, String lastName) {
+        System.out.println("First name: " + firstName);
+        System.out.println("Middle name: " + middleName);
+        System.out.println("Last name: " + lastName);
+
     }
 }
+

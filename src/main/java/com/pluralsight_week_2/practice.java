181fd2c043a11606cb9a51794e477bebbfcd1e62
Tesfahun Fola <folatesfahun@gmail.com>
2025-10-11 14:21:28 -0700

    My New update

--- src/main/java/com/pluralsight_week_2/practice.java
+++ src/main/java/com/pluralsight_week_2/practice.java
@@ -1,18 +1,26 @@
 package com.pluralsight_week_2;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class practice {
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("HighScoreWins.java");
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()){
-            String input = scanner.nextLine();
-            System.out.println(input);
+    public static void main(String[] args) throws IOException {
+
+        try {
+            FileReader file = new FileReader("src/main/java/com/pluralsight_week_2/Due_dates");
+//        Scanner scanner = new Scanner(file);
+            BufferedReader bufferedReader = new BufferedReader(file);
+//            bufferedReader.readLine();
+            String input = "";
+            while ((input = bufferedReader.readLine())!=null){
+//                String input = scanner.nextLine();
+                System.out.println(input);
+            }
+            bufferedReader.close();
+        }catch (IOException e){
+            System.out.println("No file");
         }
-        scanner.close();
+
 
 
     }

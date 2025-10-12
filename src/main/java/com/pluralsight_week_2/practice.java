ac42bb40f0e783f4580cce87e70856144f4ac961
Tesfahun Fola <folatesfahun@gmail.com>
2025-10-12 13:49:22 -0700

    My New update

--- src/main/java/com/pluralsight_week_2/practice.java
+++ src/main/java/com/pluralsight_week_2/practice.java
@@ -2,15 +2,22 @@ package com.pluralsight_week_2;
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.*;
+import javax.swing.JOptionPane;
 
 public class practice {
     public static void main(String[] args) throws IOException {
+        practice2();
+        practice1();
 
+
+
+    }
+    public static void practice1(){
         try {
             FileReader file = new FileReader("src/main/java/com/pluralsight_week_2/Due_dates");
-//        Scanner scanner = new Scanner(file);
             BufferedReader bufferedReader = new BufferedReader(file);
-//            bufferedReader.readLine();
+            bufferedReader.readLine();
             String input = "";
             while ((input = bufferedReader.readLine())!=null){
 //                String input = scanner.nextLine();
@@ -21,7 +28,8 @@ public class practice {
             System.out.println("No file");
         }
 
-
+    }
+    public static void practice2(){
 
     }
 }

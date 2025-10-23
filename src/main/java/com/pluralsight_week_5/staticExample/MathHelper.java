29b422bcca95b9c155ecc0fb9f0fb01c95638f08
Tesfahun Fola <folatesfahun@gmail.com>
2025-10-23 10:29:35 -0700

    My New update

---
+++ src/main/java/com/pluralsight_week_5/staticExample/MathHelper.java
@@ -0,0 +1,16 @@
+package com.pluralsight_week_5.staticExample;
+
+public class MathHelper {
+
+
+    public MathHelper() {
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+    public static int square(int number){
+        return number * number;
+    }
+
+}
---
+++ src/test/java/com/pluralsight_week_5/staticExample/MathHelperTest.java
@@ -0,0 +1,28 @@
+package com.pluralsight_week_5.staticExample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathHelperTest {
+
+    @Test
+    void add() {
+        //arrange
+        int total = MathHelper.add(3, 5);
+        //act
+        int expect = 8;
+        //assert
+        assertEquals(8, total);
+    }
+
+    @Test
+    void square() {
+        //arrange
+        int total = MathHelper.square(3);
+        //act
+//        int expect = 9;
+        //assert
+        assertEquals(9, total);
+    }
+}
